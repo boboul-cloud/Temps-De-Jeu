954a4c0ade89c93339bf1253e1e8954f94aa3f53
BudgetApp <bob.oulhen@gmail.com>
2026-02-06 21:51:21 +0100

    Initial Commit

---
+++ Temps De Jeu/Temps_De_JeuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temps_De_JeuApp.swift
+//  Temps De Jeu
+//
+//  Created by Robert Oulhen on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Temps_De_JeuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
